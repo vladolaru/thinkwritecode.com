809be91cfacd187cbe4833236566c29fb1935820
Vlad Olaru <vlad.olaru@pixelgrade.com>
2020-02-19 10:01:07 +0200

    More nuanced reading time

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,4 +1,9 @@
 export function formatReadingTime(minutes) {
+  // The reading time in minutes is calculated on an average rate of 200 words per minute (wpm).
+  // I want to give a little more nuance and show an interval between a non-native rate of about 150 wpm and native reader of about 250 wpm.
+
   let cups = Math.round(minutes / 5);
-  return `${new Array(cups || 1).fill('☕️').join('')} ${minutes} min read`;
+  let slowReading = Math.round(minutes * 1.25);
+  let fastReading = Math.round(minutes * 0.75);
+  return `${new Array(cups || 1).fill('☕️').join('')} ${fastReading} – ${slowReading} minutes read`;
 }
